16921eb5a2b9f8680be39902f0dbc2a8c7843f5e
Shubham Patil <shubhpatil8510@gmail.com>
2025-09-15 14:48:19 +0530

    Added a calender data.

---
+++ src/Components/Calender/data.js
@@ -0,0 +1,38 @@
+export const data = [
+  {
+    id: 1,
+    title: 'Project One Meeting',
+    startTime: '10:00:00',
+    endTime: '11:00:00'
+  },
+  {
+    id: 2,
+    title: 'Lunch with Sarah',
+    startTime: '12:30:00',
+    endTime: '13:30:00'
+  },
+  {
+    id: 3,
+    title: 'Team Sync',
+    startTime: '15:00:00',
+    endTime: '15:30:00'
+  },
+  {
+    id: 4,
+    title: 'Client Call',
+    startTime: '17:00:00',
+    endTime: '18:00:00'
+  },
+  {
+    id: 5,
+    title: 'Code Review',
+    startTime: '19:00:00',
+    endTime: '20:00:00'
+  },
+  {
+    id: 6,
+    title: 'Product Launch Planning',
+    startTime: '21:00:00',
+    endTime: '22:00:00'
+  }
+];
